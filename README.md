bf32a9333cf78917d5c87234cee634dd76425dd3
FadyEilia2001 <fadyeilia1@gmail.com>
2025-09-17 23:57:47 -0500

    Model finished and dynamic - non Responsive

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# dashboardJs
+# Notes
+.modal-wrapper.open {
+  animation: slideIn 0.6s ease forwards;
+}
+
+.modal-wrapper.close {
+  animation: slideOut 0.6s ease forwards;
+}
+
+.dim-open {
+  opacity: 1;
+  pointer-events: auto;
+}
--- script.js
+++ script.js
@@ -1,9 +1,66 @@
-// UI elements
+//! ---------------- UI ELEMENTS ----------------
+// #region UI
 const sidebarBtn = document.querySelector("#open-sidebar");
 const appWrapper = document.querySelector(".wrapper");
+const taskContainer = document.querySelector(".main-tasks");
+const createTaskBtn = document.querySelector(".btn-new");
+const dimLayer = document.querySelector(".dim-layer");
+const newTaskModal = document.querySelector(".modal");
+const modalCloseBtn = document.querySelector(".modal-close-btn");
+const modalCancelBtn = document.querySelector(".cancel-btn");
 
-// Toggle sidebar
+// #endregion
+
+//! ---------------- STATE / CONFIG ------------
+// #region STATE
+let modalOpen = false;
+
+const tasks = [];
+let newTask = {
+  title: "",
+  desc: "",
+  dueDate: "",
+  status: "",
+  recurring: false,
+};
+// #endregion
+
+//! ---------------- PURE FUNCTIONS ------------
+// #region PURE
+function toggleModal() {
+  if (!modalOpen) {
+    dimLayer.classList.add("dim-open");
+    newTaskModal.classList.add("modal-wrapper", "open");
+    modalOpen = true;
+  } else {
+    dimLayer.classList.remove("dim-open");
+    newTaskModal.classList.remove("modal-wrapper", "close");
+    modalOpen = false;
+  }
+}
+
+// #endregion
+
+//! ---------------- EVENT HANDLERS ------------
+// #region HANDLERS
+
+// open sidebar
 sidebarBtn.addEventListener("click", () => {
   appWrapper.classList.toggle("hidden");
-  console.log(appWrapper.classList);
 });
+
+// New task button
+createTaskBtn.addEventListener("click", (e) => {
+  toggleModal();
+});
+
+// close modal button
+modalCloseBtn.addEventListener("click", () => {
+  toggleModal();
+});
+
+modalCancelBtn.addEventListener("click", () => {
+  toggleModal();
+});
+
+// #endregion
